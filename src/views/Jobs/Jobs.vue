37390f37e7d566e3f2f4f5936dbffd6b55c391a2
ibrahimmaher2020 <51016531+ibrahimmaher2020@users.noreply.github.com>
2023-03-09 14:03:50 +0200

    Add files via upload

---
+++ src/views/Jobs/Jobs.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Jobs Page</h1>
+  <div v-for="job in jobs" :key="job">
+    <h2>{{ job.titel }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        { title: "It manager", id: 1, detail: "lorem" },
+        { title: "Accounts Manager", id: 2, detail: "lorem" },
+        { title: "Accounter", id: 3, detail: "lorem" },
+      ],
+    };
+  },
+};
+</script>
